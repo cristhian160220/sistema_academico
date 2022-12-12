9cd85da8b25f0ca77fa2ae220c6dbee620fb3bfb
cristhian160220 <cienfce@gmail.com>
2022-12-12 11:26:57 -0500

    rttrtr

--- include/verificar_sesion.php
+++ include/verificar_sesion.php
@@ -1,11 +1,6 @@
 <?php
-<<<<<<< HEAD
 session_start( );
 if (!isset($_SESSION['id_usu_sisacad_iesthuanta'])) {
-=======
-session_start();
-if (!isset($_SESSION['id_usu_sistema_academico'])) {
->>>>>>> 2e2ddc96b89af29d210adf0a82721cceda8b716a
    header("location: login.php");
 }
 ?>
